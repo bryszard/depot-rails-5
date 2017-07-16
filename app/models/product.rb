7e3af3711e3dc67fe76e390e5638a33d87eef48e
Piotr <pbrych@gmail.com>
2017-07-16 13:55:30 +0200

    Add validations, add tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+
+  REQUIRED_FIELDS = [:title, :description, :image_url]
+
+  validates *REQUIRED_FIELDS, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,57 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  def new_product(options = {})
+    product = Product.new(title:       options[:title] || 'My Book Title',
+                          description: options[:description] || 'yyy',
+                          price:        options[:price] || 1,
+                          image_url:    options[:image_url] || 'zzz.jpg')
+  end
+
+  test 'product attributes must not be empty' do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test 'product price must be positive' do
+    product = new_product
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal :greater_than_or_equal_to, product.errors.details[:price].first[:error]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal :greater_than_or_equal_to, product.errors.details[:price].first[:error]
+
+    product.price = 0.1
+    assert product.valid?
+  end
+
+  test 'image url' do
+    ok = %w{fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif}
+    bad = %w{fred.doc fred.gif/more fred.gif.more}
+
+    ok.each do |name|
+      assert new_product(image_url: name).valid?, "#{name} shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_product(image_url: name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
+
+  test 'product is not valid without a unique title' do
+    product = new_product(title: products(:ruby).title)
+
+    assert product.invalid?
+    assert_equal :taken, product.errors.details[:title].first[:error]
+    assert_equal [I18n.t('errors.messages.taken')], product.errors[:title]
+  end
 end
