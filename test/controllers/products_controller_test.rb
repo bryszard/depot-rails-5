b0965d129089bde86d1c193aced06822ca52fa2b
Piotr <pbrych@gmail.com>
2017-07-18 22:23:22 +0200

    Add store controller, add styling and views, add specs

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -14,6 +14,8 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get products_url
     assert_response :success
+    assert_select 'table td.list_description', minimum: 3
+    assert_select 'dt', 'Programming Ruby 1.9'
   end
 
   test "should get new" do
