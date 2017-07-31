32614e09ebf90d89e22700c6b52200d21cbf1952
Piotr <pbrych@gmail.com>
2017-07-31 08:11:27 +0200

    Add additional features

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,7 +46,9 @@ class ProductsController < ApplicationController
         format.json { render :show, status: :ok, location: @product }
 
         @products = Product.all
-        ActionCable.server.broadcast 'products', html: render_to_string('store/index', layout: false)
+        @updated_product = @product
+        ActionCable.server.broadcast 'products',
+          html: render_to_string('store/index', layout: false)
       else
         format.html { render :edit }
         format.json { render json: @product.errors, status: :unprocessable_entity }
